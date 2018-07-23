3ea1100b77263cea83f9c0158ae29577a923114f
sremedios <sam.remedios@gmail.com>
2018-07-23 19:50:26 -0400

    new figure saving

--- utils/save_figures.py
+++ utils/save_figures.py
@@ -7,6 +7,8 @@ from .utils import remove_ext
 
 # TODO: handle saving binary images
 def save_slice(filename, ct_img_data, pred_mask_img_data, gt_mask_img_data, slices_dice, result_dst):
+    # also ensure to get screencaps of these specific slices
+    specified_slices = [10, 12, 14]
 
     best_dice = 0
     worst_dice = 1
@@ -43,3 +45,20 @@ def save_slice(filename, ct_img_data, pred_mask_img_data, gt_mask_img_data, slic
         best_im.save(os.path.join(result_dst, best_slice_filename))
         worst_im.save(os.path.join(result_dst, worst_slice_filename))
 
+        # TODO: ensure this works
+        for specified_idx in specified_slices:
+            if specified_idx in [best_slice_idx, worst_slice_idx]:
+                continue
+            cur_slice = img_data[:,:,specified_idx]
+            cur_slice = cur_slice / np.max(cur_slice) * 255
+            cur_slice = cur_slice.astype(np.uint8).T
+            cur_im = Image.fromarray(cur_slice).convert('LA')
+
+            if img_data is ct_img_data:
+                cur_slice_filename = remove_ext(filename) + "_specified_slice_" + str(cur_slice_idx).zfill(2) + "_orig.png"
+            elif img_data is pred_mask_img_data:
+                cur_slice_filename = remove_ext(filename) + "_specified_slice_" + str(cur_slice_idx).zfill(2) + "_pred.png"
+            elif img_data is gt_mask_img_data:
+                cur_slice_filename = remove_ext(filename) + "_specified_slice_" + str(cur_slice_idx).zfill(2) + "_gt.png"
+
+            cur_im.save(os.path.join(result_dst, cur_slice_filename))
